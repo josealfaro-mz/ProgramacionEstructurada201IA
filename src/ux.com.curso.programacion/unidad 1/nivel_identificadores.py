476d251b9a342146e5da4672bd125b7d190668e6
José <marix_pp@hotmail.com>
2026-03-10 07:58:43 -0600

    identificadores

---
+++ src/ux.com.curso.programacion/unidad 1/nivel_identificadores.py
@@ -0,0 +1,19 @@
+# Ejemplo para visualizar la identacion en Python
+
+def explicar_indetificacion():
+    # nivel 1
+    mensaje ="Nivel 1 de indentificacion"
+    print(mensaje)
+
+    puntos =10
+
+    if puntos >9:
+        #nivel 2
+        print("Entrar al flujo de if")
+
+        if puntos ==10:
+            # Nivel 3
+            print("Puntos es igual a 10")
+
+    #cierre nivel 1
+    
