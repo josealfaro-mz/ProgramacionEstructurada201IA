19a7deff24adc33c1897e21a7b786249e4804cf2
José <marix_pp@hotmail.com>
2026-03-17 07:58:48 -0600

    Ejercicio_aritmetico_tangente_3.1

--- src/ux.com.curso.programacion/unidad 1/ejercicios_aritmeticos/ejericicios_aritmeticos_3.py
+++ src/ux.com.curso.programacion/unidad 1/ejercicios_aritmeticos/ejericicios_aritmeticos_3.py
@@ -7,23 +7,24 @@ def mostrar_funciones_math(numero):
 
     sen_x = math.sin(numero)
     conse_x = math.cos(numero)
-    # Calcular la tangente usando la función tan de math
-    tan_x = math.tan(numero)
 
     print("El seno de", numero, "es:", sen_x)
-    print("El coseno de", numero, "es:", conse_x)
-    # Imprimir la tangente calculada
-    print("La tangente de", numero, "es:", tan_x)   
+    print("El coseno de", numero, "es:", conse_x) 
 
     resultado = sen_x + conse_x ** 2
-    resultado_tan = sen_x + conse_x ** 2 + tan_x
 
     print("El resultado de sen^2 + cos^2 es:", resultado)
-    print("El resultado de sen^2 + cos^2 + tan^2 es:", resultado_tan)
+
+def tan_x(numero):
+    tan_x = math.tan(numero)
+    print("El tangente de", numero, "es:", tan_x)
+    resultado = tan_x ** 2 + 1
+    print("El resultado de tan^2 + 1 es:", resultado)
 
 def main():
     numero = float(input("Ingrese un numero: "))
     mostrar_funciones_math(numero)
+    tan_x(numero)
 
 if __name__ == "__main__":
     main()
