96b4e552b392d4b144169d20802724670452169a
José <marix_pp@hotmail.com>
2026-04-10 08:51:41 -0600

    Ejercicio_identidad_trigonometrica

--- src/ux.com.curso.programacion/unidad 2/jupiter_ejercicio_01/identidad_trigonometrica.py
+++ src/ux.com.curso.programacion/unidad 2/jupiter_ejercicio_01/identidad_trigonometrica.py
@@ -1,10 +1,26 @@
 # Calculo de Identidad Trigonometrica
 import math
 
-x= 42
-numero_radianes= math.radians(x)
-seno_x = math.sin(numero_radianes)**2
-coseno_x = math.cos(numero_radianes)**2
-identidad = seno_x + coseno_x
+def calculo_identidad(x):
+    numero_radianes= math.radians(x)
+    seno_x = math.pow(math.sin(numero_radianes), 2)
+    coseno_x = math.pow(math.cos(numero_radianes), 2)
+    identidad = seno_x + coseno_x
 
-print(f"Para x = {x} grados: (Sin x)2 + (Cos x)2 = {identidad}")
+    print(f"Para x = {x} grados: (Sin x)2 + (Cos x)2 = {identidad}")
+
+def calculo_identidad_2(x):
+    numero_radianes= math.radians(x)
+    seno_x = math.pow(math.sin(numero_radianes), 2)
+    coseno_x = math.pow(math.cos(numero_radianes), 2)
+    identidad = seno_x + coseno_x
+
+    print(f"Para x = {x} grados: (Sin x)2 + (Cos x)2 = {identidad}")
+
+def main():
+    x = int(input("Ingrese el valor de x en grados: "))
+    calculo_identidad(x)
+    calculo_identidad_2(x)
+
+if __name__ == "__main__":
+    main()
